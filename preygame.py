89ac008573b26ac0eb0f6e520ba48901b46192bd
msidikpr <65372761+msidikpr@users.noreply.github.com>
2023-01-18 10:59:02 -0800

    update

---
+++ preygame.py
@@ -0,0 +1,188 @@
+# Python code to implement Conway's Game Of Life
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+ 
+# setting up the values for the grid
+ON = 255
+PRED = 100 
+OFF = 0
+vals = [ON, PRED ,OFF]
+ 
+def randomGrid(N):
+ 
+    """returns a grid of NxN random values"""
+    return np.random.choice(vals, N*N, p=[0.2,0.2, 0.6]).reshape(N, N)
+
+def setaliveGrid(N,alive):
+    """returns a grid of NXN with set alive cells"""
+    temp = np.zeros((N,N))
+    shape = temp.shape
+    temp = temp.flatten()
+    inds = np.random.choice(temp.size, size=alive)
+    temp[inds] = np.full(shape = alive,fill_value = ON, dtype=np.int)
+    temp = temp.reshape(shape)                     # Restore original shape
+    return temp  
+
+
+    
+    
+ 
+def addGlider(i, j, grid):
+ 
+    """adds a glider with top left cell at (i, j)"""
+    glider = np.array([[0,    0, 255],
+                       [255,  0, 255],
+                       [0,  255, 255]])
+    grid[i:i+3, j:j+3] = glider
+ 
+def addGosperGliderGun(i, j, grid):
+ 
+    """adds a Gosper Glider Gun with top left
+       cell at (i, j)"""
+    gun = np.zeros(11*38).reshape(11, 38)
+ 
+    gun[5][1] = gun[5][2] = 255
+    gun[6][1] = gun[6][2] = 255
+ 
+    gun[3][13] = gun[3][14] = 255
+    gun[4][12] = gun[4][16] = 255
+    gun[5][11] = gun[5][17] = 255
+    gun[6][11] = gun[6][15] = gun[6][17] = gun[6][18] = 255
+    gun[7][11] = gun[7][17] = 255
+    gun[8][12] = gun[8][16] = 255
+    gun[9][13] = gun[9][14] = 255
+ 
+    gun[1][25] = 255
+    gun[2][23] = gun[2][25] = 255
+    gun[3][21] = gun[3][22] = 255
+    gun[4][21] = gun[4][22] = 255
+    gun[5][21] = gun[5][22] = 255
+    gun[6][23] = gun[6][25] = 255
+    gun[7][25] = 255
+ 
+    gun[3][35] = gun[3][36] = 255
+    gun[4][35] = gun[4][36] = 255
+ 
+    grid[i:i+11, j:j+38] = gun
+ 
+def update(frameNum, img, grid, N):
+ 
+    # copy grid since we require 8 neighbors
+    # for calculation and we go line by line
+    newGrid = grid.copy()
+    predGrid = grid.copy()
+    predGrid[predGrid == 255] = 0
+    preyGrid = grid.copy()
+    preyGrid[preyGrid == 100] = 0
+
+    for i in range(N):
+        for j in range(N):
+ 
+            # compute 8-neighbor sum
+            # using toroidal boundary conditions - x and y wrap around
+            # so that the simulation takes place on a toroidal surface.
+        
+            PREYtotal = int((preyGrid[i, (j-1)%N] + preyGrid[i, (j+1)%N] +
+                         preyGrid[(i-1)%N, j] + preyGrid[(i+1)%N, j] +
+                         preyGrid[(i-1)%N, (j-1)%N] + preyGrid[(i-1)%N, (j+1)%N] +
+                         preyGrid[(i+1)%N, (j-1)%N] + preyGrid[(i+1)%N, (j+1)%N])/255)
+            
+
+            PREDtotal = int((predGrid[i, (j-1)%N] + predGrid[i, (j+1)%N] +
+                         predGrid[(i-1)%N, j] + predGrid[(i+1)%N, j] +
+                         predGrid[(i-1)%N, (j-1)%N] + predGrid[(i-1)%N, (j+1)%N] +
+                         predGrid[(i+1)%N, (j-1)%N] + predGrid[(i+1)%N, (j+1)%N])/100)
+ 
+            # apply Conway's rules
+            if preyGrid[i, j]  == ON:
+                if  (PREDtotal >= 2):
+                    newGrid[i, j] = OFF
+                if (PREYtotal < 2) or (PREYtotal > 3):
+                    newGrid[i, j] = OFF  
+                
+
+            if grid[i, j]  == OFF:
+                if (PREYtotal == 3):
+                    newGrid[i, j] = ON
+                if(PREDtotal == 3):
+                    newGrid[i, j] = PRED
+
+            if predGrid[i, j]  == PRED:
+                if (PREYtotal >=2):
+                    newGrid[i, j] = OFF
+                if (PREDtotal < 2) or (PREDtotal > 3):
+                    newGrid[i, j] = OFF
+ 
+    # update data
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    return img,
+ 
+# main() function
+def main():
+ 
+    # Command line args are in sys.argv[1], sys.argv[2] ..
+    # sys.argv[0] is the script name itself and can be ignored
+    # parse arguments
+    parser = argparse.ArgumentParser(description="Runs Conway's Game of Life simulation.")
+ 
+    # add arguments
+    parser.add_argument('--grid-size', dest='N', required=False)
+    parser.add_argument('--numalive', dest ='alive', required=False)
+    parser.add_argument('--mov-file', dest='movfile', required=False)
+    parser.add_argument('--interval', dest='interval', required=False)
+    parser.add_argument('--glider', action='store_true', required=False)
+    parser.add_argument('--gosper', action='store_true', required=False)
+    parser.add_argument('--setalive', action='store_true', required=False)
+    args = parser.parse_args()
+     
+    # set grid size
+    N = 100
+
+    if args.N and int(args.N) > 8:
+        N = int(args.N)
+         
+    # set animation update interval
+    updateInterval = 50
+    if args.interval:
+        updateInterval = int(args.interval)
+ 
+    # declare grid
+    grid = np.array([])
+ 
+    # check if "glider" demo flag is specified
+    if args.glider:
+        grid = np.zeros(N*N).reshape(N, N)
+        addGlider(1, 1, grid)
+    elif args.gosper:
+        grid = np.zeros(N*N).reshape(N, N)
+        addGosperGliderGun(10, 10, grid)
+    elif args.setalive:
+        alive = int(args.alive)
+        grid = setaliveGrid(N,alive)
+
+ 
+    else:   # populate grid with random on/off -
+            # more off than on
+        grid = randomGrid(N)
+ 
+    # set up animation
+    fig, ax = plt.subplots()
+    img = ax.imshow(grid, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
+                                  frames = 10,
+                                  interval=updateInterval,
+                                  save_count=50)
+ 
+    # # of frames?
+    # set output file
+    if args.movfile:
+        ani.save(args.movfile, fps=30, extra_args=['-vcodec', 'libx264'])
+ 
+    plt.show()
+ 
+# call main
+if __name__ == '__main__':
+    main()
